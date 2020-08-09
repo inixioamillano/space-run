10d5f4c9850a53b77ca96ca77448b0937d522810
Inixio <inixio97@gmail.com>
2020-08-09 19:24:11 +0200

    Updates

---
+++ precache-manifest.35db08b51970eba061a6acf516b9e859.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9125b09c4f4e6593dabca13c63bd87",
+    "url": "/space-run/index.html"
+  },
+  {
+    "revision": "03acbfbd25b13c2de5b5",
+    "url": "/space-run/static/css/2.0917e70b.chunk.css"
+  },
+  {
+    "revision": "ceb40634936f91e4e424",
+    "url": "/space-run/static/css/main.101c7349.chunk.css"
+  },
+  {
+    "revision": "03acbfbd25b13c2de5b5",
+    "url": "/space-run/static/js/2.2aaf9a9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/space-run/static/js/2.2aaf9a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb40634936f91e4e424",
+    "url": "/space-run/static/js/main.fc54d2a7.chunk.js"
+  },
+  {
+    "revision": "9f0415f3d7b8f8a7aae3",
+    "url": "/space-run/static/js/runtime-main.5e48aaed.js"
+  },
+  {
+    "revision": "377156c0fad7059ce5cf355b8e7c2ca2",
+    "url": "/space-run/static/media/New Year.377156c0.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/space-run/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/space-run/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/space-run/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/space-run/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/space-run/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
